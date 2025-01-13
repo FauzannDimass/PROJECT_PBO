bf6c605c3bbf52c2ad256a19aaf597a14f5e39dc
FauzannDimass <2300018427@webmail.uad.ac.id>
2025-01-13 20:15:21 +0700

    Update Member.java

--- Member.java
+++ Member.java
@@ -2,11 +2,20 @@ abstract class Member {
     private int memberId;
     private String name;
     private String role;
+    private Team team;
 
     public Member(int memberId, String name, String role) {
         this.memberId = memberId;
         this.name = name;
         this.role = role;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public Team getTeam() {
+        return team;
     }
 
     @Override
