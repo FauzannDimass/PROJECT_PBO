317f2f63c71230fb6e9d8ce01b685b92288e4922
FauzannDimass <2300018427@webmail.uad.ac.id>
2025-01-13 20:10:56 +0700

    Update Anggaran.java

--- Anggaran.java
+++ Anggaran.java
@@ -1,12 +1,10 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class Anggaran {
+public class Anggaran {
     private int idAnggaran;
     private double totalAnggaran;
     private double anggaranTerealisasi;
     private double anggaranTersisa;
     private List<Material> materials;
+    private Projek proyek;
 
     public Anggaran(int idAnggaran, double totalAnggaran) {
         this.idAnggaran = idAnggaran;
@@ -14,9 +12,22 @@ class Anggaran {
         this.materials = new ArrayList<>();
     }
 
+    public void setProyek(Projek proyek) {
+        this.proyek = proyek;
+    }
+
+    public Projek getProyek() {
+        return proyek;
+    }
+
     public void tambahMaterial(Material material) {
         materials.add(material);
         hitungAnggaranTerealisasi();
+    }
+
+    public void tambahMaterial(String jenisMaterial, String namaMaterial, int kuantitas, double hargaMaterial) {
+        Material material = new Material(jenisMaterial, namaMaterial, kuantitas, hargaMaterial);
+        tambahMaterial(material);
     }
 
     private void hitungAnggaranTerealisasi() {
